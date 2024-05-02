66e7c330bc163bc42772e04da01fb957fbff21a6
basia <basia@PC-002>
2024-05-02 14:52:25 +0200

    Dodaj pliki projektów.

---
+++ AgentForFabric/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using Library;
+
+class Program
+{
+     static async Task Main(string[] args)
+     {
+
+          Console.WriteLine("Witaj w AgentForFabric ! ");
+          Console.WriteLine("-------------------------------------------------");
+
+
+      
+          // AZURE podpięcie się do urządzenia w hubZajecia o nazwie test_device
+          Console.WriteLine(" !!!            ---  Łączenie z  Azure !");
+          // String do połączenia z Azure wpisane 
+          Console.WriteLine("Wpisz string do połączenia z Azure  ( Device Numbers ) : ");
+          string deviceConnectionString = Console.ReadLine() ?? string.Empty;
+          using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+          await deviceClient.OpenAsync();
+          Console.WriteLine(" !!!          Łączenie z Azure zakończone sukcesem !");
+
+ 
+
+
+          
+          Console.WriteLine("AgentForFabric zatrzymana   ");
+          Console.ReadLine();
+
+     }
+}
---
+++ Library/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Client;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Azure.Devices.Shared;
+
+
+namespace Library
+{
+     public class Class1
+     {
+          private DeviceClient client;
+          private OpcClient OPC;
+
+
+          public Class1(DeviceClient deviceClient, OpcClient OPC)
+          {
+               this.client = deviceClient;
+               this.OPC = OPC;
+          }
+
+
+  
+
+
+     }
+}
